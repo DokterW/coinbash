db45cd618c09ba2aa8ebacbb26b935d4f0ca5309
Dr. Waldijk <DokterW@users.noreply.github.com>
2017-05-07 20:02:49 +0200

    coinbash

--- README.md
+++ README.md
@@ -1,14 +1,17 @@
-# shETHer
+# coinbash
 
-Get the current rate for ETH ([Ethereum](https://www.ethereum.org)) from [Coinbase](https://www.coinbase.com).
-
-It's made to fit in a 22x9 window.
+Get the current rate for BTC, ETH & LTC from [Coinbase](https://www.coinbase.com).
 
 ### Roadmap
 
-*empty*
+* Coins will be saved to file instead of memory.
+* Improved menu system.
 
 ### Changelog
+
+#### 2017-05-07
+* Changed the name from shETHer to coinbash as I will be including all three currencies from Coinbase -- BTC, ETH & LTC.
+* Removed currency change indicator till I can find a good way to make it work for all currency without adding too much lines of code than necessary.
 
 #### 2017-04-27
 * To calculate if the rate had gone up or down, for the comparison to work I had to remove the decimals, which didn't look so well if it indicated no change but new rate is 400.54 and old rate was 400.45. Now it removes the decimal separator.
--- start.sh
+++ start.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-# shETHer v0.4
+# coinbash v0.6
 # Made by Dr. Waldijk
-# A simple script that fetches ETH rate from coinbase.com.
+# A simple script that fetches BTC, ETH  & LTC currency rate from coinbase.com.
 # Read the README.md for more info, but you will find more info here below.
 # By running this script you agree to the license terms.
 # Config ----------------------------------------------------------------------------
-ETHNAM="shETHer"
-ETHVER="0.4"
-ETHOLD="-"
-ETHIND="-"
+CIBNAM="coinbash"
+CIBVER="0.6"
+CIBLOC="$HOME/.dokter/coinbash"
+CIBOLD="0"
+CIBIND="-"
 # Refresh (in seconds)
-ETHTIM="600"
+CIBTIM="600"
 # Install dependencies --------------------------------------------------------------
 if [ ! -e /usr/bin/curl ] && [ ! -e /usr/bin/jq ]; then
     sudo dnf -y install curl jq
@@ -21,7 +22,7 @@ elif [ ! -e /usr/bin/jq ]; then
 fi
 # -----------------------------------------------------------------------------------
 clear
-echo "$ETHNAM - v$ETHVER"
+echo "$CIBNAM - v$CIBVER"
 echo ""
 echo "1. USD"
 echo "2. NOK"
@@ -29,57 +30,74 @@ echo "3. SEK"
 echo "4. EUR"
 echo ""
 echo ""
-read -p "Pick currency: " -s -n1 ETHKEY
-case "$ETHKEY" in
+read -p "Pick currency: " -s -n1 CIBKEY
+case "$CIBKEY" in
     1)
-        ETHCUR="USD"
+        CIBCUR="USD"
     ;;
     2)
-        ETHCUR="NOK"
+        CIBCUR="NOK"
     ;;
     3)
-        ETHCUR="SEK"
+        CIBCUR="SEK"
     ;;
     4)
-        ETHCUR="EUR"
+        CIBCUR="EUR"
     ;;
 esac
 clear
-echo "$ETHNAM - v$ETHVER"
+echo "$CIBNAM - v$CIBVER"
 echo ""
 echo ""
-echo "How much ETH"
-echo "do you have?"
+echo "How much do you have?"
 echo ""
-echo ""
-echo ""
-read -p "> " ETHCON
-ETHDEC=$(echo "scale=2; $ETHCON/1" | bc)
+read -p "BTC: " CIBCONBTC
+CIBDECBTC=$(echo "scale=2; $CIBCONBTC/1" | bc)
+read -p "ETH: " CIBCONETH
+CIBDECETH=$(echo "scale=2; $CIBCONETH/1" | bc)
+read -p "LTC: " CIBCONLTC
+CIBDECLTC=$(echo "scale=2; $CIBCONLTC/1" | bc)
 while :; do
-    ETHRAT=$(curl -s "https://api.coinbase.com/v2/exchange-rates?currency=ETH" | jq -r ".data.rates.$ETHCUR")
-    ETHVAL=$(echo "scale=2; $ETHRAT*$ETHCON/1" | bc)
+    # BTC
+    CIBRATBTC=$(curl -s "https://api.coinbase.com/v2/exchange-rates?currency=BTC" | jq -r ".data.rates.$CIBCUR")
+    CIBVALBTC=$(echo "scale=2; $CIBRATBTC*$CIBCONBTC/1" | bc)
+    # ETH
+    CIBRATETH=$(curl -s "https://api.coinbase.com/v2/exchange-rates?currency=ETH" | jq -r ".data.rates.$CIBCUR")
+    CIBVALETH=$(echo "scale=2; $CIBRATETH*$CIBCONETH/1" | bc)
     # Comparisons with decimals doesn't work, so I decided to solve it by removing the decimal separator.
-    ETHRATINT=$(echo "$ETHRAT" | sed 's/\.//g')
-    ETHOLDINT=$(echo "$ETHOLD" | sed 's/\.//g')
-    if [ "$ETHRATINT" -eq "$ETHOLDINT" ]; then
-        ETHIND="-"
-    elif [ "$ETHRATINT" -gt "$ETHOLDINT" ]; then
-        ETHIND="↑"
-    elif [ "$ETHRATINT" -lt "$ETHOLDINT" ]; then
-        ETHIND="↓"
-    fi
+#    CIBRATETHINT=$(echo "$CIBRATETH" | sed 's/\.//g')
+#    CIBOLDINT=$(echo "$CIBOLD" | sed 's/\.//g')
+#    if [ "$CIBOLDINT" -eq "0" ]; then
+#        CIBIND="-"
+#        CIBOLD="-"
+#    elif [ "$CIBRATETHINT" -eq "$CIBOLDINT" ]; then
+#        CIBIND="-"
+#    elif [ "$CIBRATETHINT" -gt "$CIBOLDINT" ]; then
+#        CIBIND="↑"
+#    elif [ "$CIBRATETHINT" -lt "$CIBOLDINT" ]; then
+#        CIBIND="↓"
+#    fi
+    # LTC
+    CIBRATLTC=$(curl -s "https://api.coinbase.com/v2/exchange-rates?currency=LTC" | jq -r ".data.rates.$CIBCUR")
+    CIBVALLTC=$(echo "scale=2; $CIBRATLTC*$CIBCONLTC/1" | bc)
     clear
-    echo "$ETHNAM - v$ETHVER"
+    echo "$CIBNAM - v$CIBVER"
     echo ""
-    echo " Rate: $ETHRAT $ETHCUR"
-    echo "   $ETHIND  ($ETHOLD $ETHCUR)"
+    echo "  BTC: $CIBRATBTC $CIBCUR"
+    echo "  ETH: $CIBRATETH $CIBCUR"
+#    echo "   $CIBIND  ($CIBOLD $CIBCUR)"
+    echo "  LTC: $CIBRATLTC $CIBCUR"
     echo ""
-    echo "Coins: $ETHDEC ETH"
-    echo "  Xch: $ETHVAL $ETHCUR"
+    echo "Coins: $CIBDECBTC BTC"
+    echo "       $CIBVALBTC $CIBCUR"
+    echo "Coins: $CIBDECETH ETH"
+    echo "       $CIBVALETH $CIBCUR"
+    echo "Coins: $CIBDECLTC LTC"
+    echo "       $CIBVALLTC $CIBCUR"
     echo ""
-    read -t $ETHTIM -s -n1 -p "(Q)uit (*)refresh " ETHKEY
-    ETHOLD=$ETHRAT
-    case "$ETHKEY" in
+    read -t $CIBTIM -s -n1 -p "(Q)uit (*)refresh " CIBKEY
+    CIBOLD=$CIBRATETH
+    case "$CIBKEY" in
         [qQ])
             echo ""
             clear
